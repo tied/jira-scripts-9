120fc5a4267bd692188a3206374ce0db337a0658
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2022-01-21 11:06:43 +0100

    Create assigneeApprover.groovy

---
+++ assigneeApprover.groovy
@@ -0,0 +1,6 @@
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.CustomFieldManager;
+import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.jql.parser.JqlQueryParser;
+import com.atlassian.jira.bc.issue.search.SearchService
