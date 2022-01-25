5f7fcc7042d3e296ed9de922c4b175301f07681b
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2022-01-25 16:51:46 +0100

    Create amountIsNotExceeded-condition.groovy

---
+++ training-request-flow/amountIsNotExceeded-condition.groovy
@@ -0,0 +1,12 @@
+import com.atlassian.jira.issue.CustomFieldManager;
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.IssueManager;
+
+def customFieldManager = ComponentAccessor.getCustomFieldManager()
+def issueManager = ComponentAccessor.getIssueManager()
+
+def currIssue = issueManager.getIssueObject(issue.key)
+def excedeedAmount = customFieldManager.getCustomFieldObject(11605) // Exceeded Amount field ID
+def excedeedAmountValue = currIssue.getCustomFieldValue(excedeedAmount) as float
+
+excedeedAmountValue <= 0.0
